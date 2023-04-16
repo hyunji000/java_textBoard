55a7dcb6b0679e2f1cc9101784fccff8b9b7c7e4
hyunji000 <kimhunji038@gmail.com>
2023-04-16 21:33:47 +0900

    스캐너생성

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //스캐너 객체
+        Scanner scanner = new Scanner(System.in);
+        //사용자에게 입력받을수 있도록 출력
+        System.out.print("명령어를 입력해 주세요 : ");
+        //값 입력 받는거
+        String command = scanner.nextLine();
+        System.out.println("입력하신 명령어는 : " + command);
     }
 
 }
