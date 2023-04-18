9ebed7854ef7658223d8fea415ba30e41920c340
hyunji000 <kimhunji038@gmail.com>
2023-04-18 21:51:24 +0900

    post 작성기능추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,10 +12,21 @@ public class Main {
         //반복문생성
         Scanner scanner = new Scanner(System.in);
         boolean programStatus = true;
+        int postCount = 0;
         while(programStatus){
             System.out.print("명령어를 입력해 주세요 : ");
             String command = scanner.nextLine().trim();
             switch (command){
+                case  "write":
+                    System.out.println("게시글을 작성합니다.");
+                    System.out.print("제목 : " );
+                    String title = scanner.nextLine().trim();
+                    System.out.print("내용 : " );
+                    String body = scanner.nextLine().trim();
+                    postCount++;
+                    Post newPost = new Post(postCount,title,body);
+                    System.out.println("작성된 게시글 : " + newPost);
+                    break;
                 case "exit":
                     System.out.println("프로그램을 종료합니다.");
                     programStatus = false;
@@ -35,3 +46,10 @@ public class Main {
 //         } else {
 //         System.out.println("명령 다시");
 //         }
+
+// git 에 올리는 순서
+//    git status
+//    git add .
+//    git status
+//    git commit -m"post 엔티티클래스 생성"
+//    git push origin master
